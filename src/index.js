b3d7cc6c0b4df81633862e4b964961d74eab3092
Ayyan Ahmed <ayyanahmed@outlook.com>
2024-10-11 22:12:16 +0500

    Database Connection Established

---
+++ src/index.js
@@ -0,0 +1,30 @@
+// require('dotenv').config({path: './env'})
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+  path: "./env",
+});
+
+connectDB();
+/*
+import express from "express";
+const app = express();
+
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_name}`);
+
+    app.on("error", (error) => {
+      console.log("Database cant be connected to", error);
+    });
+
+    app.listen(process.env.PORT, () => {
+      console.log(`App is listening on port ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.error("Error:", error);
+    throw error;
+  }
+})();
+*/
